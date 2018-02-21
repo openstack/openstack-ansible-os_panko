0e01d050c4bf13bdb3abaf1308d98de74c5bb3d8
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-02-21 11:41:27 +0000

    Update branch for master, and add scaffolding for openstack-infra
    The content in master came from the stable/pike branch, so we change
    all the branch references to work with master instead.
    
    We also add various scaffolding and update various files so that
    it matches the right content for master and will be able to build
    docs/releasenotes when imported into openstack-infra.

--- tests/tests-repo-clone.sh
+++ tests/tests-repo-clone.sh
@@ -96,7 +96,7 @@ elif [[ ! -d tests/common ]]; then
     # Otherwise we're clearly not in zuul or using a previously setup
     # repo in some way, so just clone it from upstream.
     else
-        git clone -b stable/pike \
+        git clone \
             https://git.openstack.org/openstack/openstack-ansible-tests \
             ${WORKING_DIR}/tests/common
     fi
@@ -110,7 +110,7 @@ fi
 # tests repo are not supported.
 if [[ "${CLONE_UPGRADE_TESTS}" == "yes" ]]; then
     if [[ ! -d "${WORKING_DIR}/tests/common/previous" ]]; then
-        git clone -b stable/ocata \
+        git clone -b stable/queens \
             https://git.openstack.org/openstack/openstack-ansible-tests \
             ${WORKING_DIR}/tests/common/previous
   fi
